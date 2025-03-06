95dbde46898b8f904dec8f9db53b6ed1305370af
digkill <digkill@mail.ru>
2025-03-06 21:53:07 +0500

    Added functional

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,149 @@
-package cmd
+package main
+
+import (
+	"context"
+	"errors"
+	config "github.com/digkill/news-grabber-bot/internal"
+	"github.com/digkill/news-grabber-bot/internal/bot"
+	"github.com/digkill/news-grabber-bot/internal/bot/middleware"
+	"github.com/digkill/news-grabber-bot/internal/botkit"
+	"github.com/digkill/news-grabber-bot/internal/fetcher"
+	"github.com/digkill/news-grabber-bot/internal/notifier"
+	"github.com/digkill/news-grabber-bot/internal/storage"
+	"github.com/digkill/news-grabber-bot/internal/summary"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("[ERROR] failed to create botAPI: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("[ERROR] failed to connect to db: %v", err)
+		return
+	}
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("[ERROR] failed to close db: %v", err)
+		}
+	}(db)
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.NewFetcher(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		summarizer = summary.NewOpenAISummarizer(
+			config.Get().OpenAIKey,
+			config.Get().OpenAIModel,
+			config.Get().OpenAIPrompt,
+		)
+		notifier = notifier.NewNotifier(
+			articleStorage,
+			summarizer,
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	newsBot := botkit.NewBot(botAPI)
+	newsBot.RegisterCmdView(
+		"addsource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdAddSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"setpriority",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdSetPriority(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"getsource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdGetSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"listsources",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdListSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"deletesource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdDeleteSource(sourceStorage),
+		),
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run fetcher: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] fetcher stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := notifier.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run notifier: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] notifier stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := http.ListenAndServe("9.0.0.0:8080", mux); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run http server: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] http server stopped")
+		}
+	}(ctx)
+
+	if err := newsBot.Run(ctx); err != nil {
+		log.Printf("[ERROR] failed to run botkit: %v", err)
+	}
+}
