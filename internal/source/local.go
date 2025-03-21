80884f897ec5a4d01474e491725ad52c7d2caf0d
Digkill <357629-digkill@users.noreply.gitlab.com>
2025-03-21 19:34:57 +0500

    Added storage

--- internal/source/local.go
+++ internal/source/local.go
@@ -3,42 +3,43 @@ package source
 import (
 	"context"
 	"github.com/digkill/news-grabber-bot/internal/models"
-	"strings"
-
-	"github.com/SlyMarbo/rss"
-	"github.com/samber/lo"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 type LocalSource struct {
-	URL        string
+	File       *os.File
 	SourceID   int64
 	SourceName string
 }
 
-func LocalSourceFromModel(m models.Source) LocalSource {
+func NewLocalSourceFromModel(m models.File) LocalSource {
 	return LocalSource{
-		URL:        m.FeedURL,
-		SourceID:   m.ID,
-		SourceName: m.Name,
+		File:       m.File,
+		SourceID:   m.SourceID,
+		SourceName: m.SourceName,
 	}
 }
 
-func (s LocalSource) Fetch(ctx context.Context) ([]models.Item, error) {
+func (s LocalSource) Fetch(ctx context.Context) (*os.File, error) {
 	feed, err := s.loadFeed(ctx, s.URL)
 	if err != nil {
 		return nil, err
 	}
 
-	return lo.Map(feed.Items, func(item *rss.Item, _ int) models.Item {
-		return models.Item{
-			Title:      item.Title,
-			Link:       item.Link,
-			Categories: item.Categories,
-			Date:       item.Date,
-			SourceName: s.SourceName,
-			Summary:    strings.TrimSpace(item.Summary),
-		}
-	}), nil
+	return feed, nil
+
+	//	return lo.Map(feed.Items, func(item *rss.Item, _ int) models.Item {
+	//		return models.Item{
+	//			Title:      item.Title,
+	//			Link:       item.Link,
+	//			Categories: item.Categories,
+	//			Date:       item.Date,
+	//			SourceName: s.SourceName,
+	//			Summary:    strings.TrimSpace(item.Summary),
+	//		}
+	//	}), nil
 }
 
 func (s LocalSource) ID() int64 {
@@ -49,12 +50,28 @@ func (s LocalSource) Name() string {
 	return s.SourceName
 }
 
-func (s LocalSource) loadFeed(ctx context.Context, url string) (*rss.Feed, error) {
-	var feedCh = make(chan *rss.Feed)
+func (s LocalSource) loadFeed(ctx context.Context, url string) (*os.File, error) {
+	var feedCh = make(chan *os.File)
 	var errCh = make(chan error)
 
+	imageName := "image.gif" // имя файла картинки
+	imagePath := filepath.Join("./internal/storage/images", imageName)
+
+	// Открываем файл
+	file, err := os.Open(imagePath)
+	if err != nil {
+		log.Println("Ошибка открытия картинки:", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Println("Ошибка закрытие файла:", err)
+		}
+	}(file)
+
 	go func() {
-		feed, err := rss.Fetch(url)
+
+		feed := file
 		if err != nil {
 			errCh <- err
 			return
