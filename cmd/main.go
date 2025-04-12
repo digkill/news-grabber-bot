baaadaa77c49da89bc18de5c9d6762fd66a62c7e
Digkill <357629-digkill@users.noreply.gitlab.com>
2025-04-12 16:49:51 +0500

    Added storage

--- cmd/main.go
+++ cmd/main.go
@@ -58,7 +58,7 @@ func main() {
 		)
 
 		newPoster = poster.NewPoster(
-			"storage/images",
+			"./internal/storage/images",
 			config.Get().NotificationInterval,
 			botAPI,
 			config.Get().TelegramChannelID,
